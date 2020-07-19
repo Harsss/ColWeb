7c827ae25de91c525cba4e5cd83640345dd23981
Harsh Bindal <harshbin06@gmail.com>
2020-07-19 21:26:43 +0530

    Basic Homepage

---
+++ index1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php Tutorial</title>
+</head>
+<body>
+    <div class="container">
+
+    This is my first php programme
+<?php 
+// this is the way to define constants
+define('pi',3.14);
+// echo "<br>" + print using php;
+$var1 = "hello";
+$var2=67;
+echo "<br>";
+echo var_dump($var1==$var2);
+
+// and --> &&
+// or ||
+// xor (both true or false then false and then true  )
+// not !
+
+// datatypes
+// strings integers, float, boolean array, object
+
+echo "datatypes of php";
+?>
+
+
+    </div>
+</body>
+</html>
